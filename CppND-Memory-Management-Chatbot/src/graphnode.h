60bd7023b18f611c55241fa057bc714671f7b65c
Christine417 <74065675+Christine417@users.noreply.github.com>
2021-06-25 00:35:06 -0400

    Update graphnode.h

--- CppND-Memory-Management-Chatbot/src/graphnode.h
+++ CppND-Memory-Management-Chatbot/src/graphnode.h
@@ -4,10 +4,12 @@
 #include <vector>
 #include <string>
 #include "chatbot.h"
+#include <memory>
 
 
 // forward declarations
 class GraphEdge;
+class ChatLogic; // forward declaration
 
 class GraphNode
 {
@@ -16,11 +18,13 @@ private:
     ////
 
     // data handles (owned)
-    std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    //std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges;  // edges to subsequent nodes
 
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
-    ChatBot *_chatBot;
+    // ChatBot *_chatBot;
+    ChatBot _chatBot;
 
     ////
     //// EOF STUDENT CODE
@@ -44,12 +48,14 @@ public:
     // proprietary functions
     void AddToken(std::string token); // add answers to list
     void AddEdgeToParentNode(GraphEdge *edge);
-    void AddEdgeToChildNode(GraphEdge *edge);
+    //void AddEdgeToChildNode(GraphEdge *edge);
 
     //// STUDENT CODE
     ////
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
 
-    void MoveChatbotHere(ChatBot *chatbot);
+    // void MoveChatbotHere(ChatBot *chatbot);
+    void MoveChatbotHere(ChatBot chatbot);
 
     ////
     //// EOF STUDENT CODE
